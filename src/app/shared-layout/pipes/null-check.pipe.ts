738096370a084d86f8ed6933937cf43fa5a4a3a4
Segii Antropov <homoantropos@gmail.com>
2022-06-04 02:17:56 +0300

    testTask is ready

--- src/app/shared-layout/pipes/null-check.pipe.ts
+++ src/app/shared-layout/pipes/null-check.pipe.ts
@@ -1,13 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {Product} from "../model/interfaces";
 
 @Pipe({
   name: 'nullCheck'
 })
 export class NullCheckPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: string, key: string, product: Product): string {
     if(value === null) {
       return "it's unknown"
+    } else if(key === 'VoDatum') {
+      value = new Date(product[key]).toLocaleDateString('de-DE');
+    } else if(key === 'Price') {
+      value = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(product.Price);
     }
     return value;
   }
