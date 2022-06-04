601fcf5ac4b839a97af1b777d62b84a919c7e2db
Segii Antropov <homoantropos@gmail.com>
2022-06-04 20:50:51 +0300

    search pipe added

---
+++ src/app/shared-layout/pipes/search.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+  transform(list: Array<any>, searchValue: string, searchField: Array<string>): Array<any> {
+    if (searchValue.trim() === undefined){
+      return list;
+    }
+    if (searchField.length === 1){
+      return list.filter(a => a[searchField[0]].toString().toLowerCase().includes(searchValue.toString().toLowerCase()));
+    }
+    if (searchField.length === 2){
+      return list.filter(a => a[searchField[0]][searchField[1]].toLowerCase().includes(searchValue.toLowerCase()));
+    }
+    else {
+      return list.filter(a =>
+        a[searchField[0]][searchField[1]][searchField[2]].toLowerCase().includes(searchValue.toLowerCase()));
+    }
+  }
+}
