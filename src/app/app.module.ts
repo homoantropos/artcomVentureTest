9ed1df1b22d1fe79392ffcdea236517f87c47e76
Segii Antropov <homoantropos@gmail.com>
2022-06-05 13:21:10 +0300

    main-layout and shared-layout modules are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+
 import {MainLayoutModule} from "./main-layout/main-layout.module";
 import {SharedLayoutModule} from "./shared-layout/shared-layout.module";
 
